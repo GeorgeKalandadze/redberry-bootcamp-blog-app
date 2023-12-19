f50b7a29b2506ee1abe1518f80667a537832fcbb
George Kalandadze <kalandadzeg777@gmail.com>
2023-12-20 01:45:01 +0400

    feat:make context api

---
+++ src/context/Context.jsx
@@ -0,0 +1,22 @@
+import { createContext } from "react";
+
+
+const AppContext = createContext({
+
+})
+
+
+export const AppProvider = ({children}) => {
+
+    return (
+      <AppContext.Provider
+        
+      >
+        {children}
+      </AppContext.Provider>
+    );
+}
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
